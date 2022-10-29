2f7ef38610d44ec5a0d019a54a9f1b87c884f681
elghibaomar <elghibaomar@gmail.com>
2022-10-29 21:10:21 +0100

    v1

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueApexCharts from 'vue-apexcharts';
+
+
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000/'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.component('apexchart', VueApexCharts);
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  store,
+  router,
+  axios,
+  render: h => h(App),
+});
